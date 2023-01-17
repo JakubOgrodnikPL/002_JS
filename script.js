307cde0b65ab9f2d38de728c3e0b5acecb6b2ac2
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2023-01-17 05:48:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// alert('Siema');
+
+const name = "Jakub";
+let pet ="Goljad";
+let petAge = 4;
+
+console.log(`Cześć. Jestem ${name}, a to jest mój pies ${pet}.`);
+console.log('Mój pies ' + pet + ' ma już ' + petAge + ' lata.');
